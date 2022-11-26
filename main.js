e8398dec968bb530401d446f621bbcf800782ba3
Spyrius <61468175+Lubo77@users.noreply.github.com>
2022-11-26 22:36:59 +0100

    first version is done

---
+++ main.js
@@ -0,0 +1,8 @@
+let btnDemo = document.querySelector('.hero-left__btn');
+let bannerChecked = document.querySelector('.hero-left__checked-banner');
+
+btnDemo.addEventListener('click', () => {
+    btnDemo.classList.toggle('hide');
+    bannerChecked.classList.toggle('show');
+    // btnDemo.innerText = "Your request has been sent";
+})
